a0bad361c45ae5a5fb9969e4167991219ab923c6
infamous55 <infamouslegend555@gmail.com>
2023-06-16 20:51:01 +0300

    updated database types

--- src/lib/database.types.ts
+++ src/lib/database.types.ts
@@ -4,24 +4,48 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json }
-  | Json[]
+  | Json[];
 
 export interface Database {
   public: {
     Tables: {
-      [_ in never]: never
-    }
+      repo_stats: {
+        Row: {
+          enabled: boolean | null;
+          id: number;
+          user_id: string | null;
+        };
+        Insert: {
+          enabled?: boolean | null;
+          id?: number;
+          user_id?: string | null;
+        };
+        Update: {
+          enabled?: boolean | null;
+          id?: number;
+          user_id?: string | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'repo_stats_user_id_fkey';
+            columns: ['user_id'];
+            referencedRelation: 'users';
+            referencedColumns: ['id'];
+          }
+        ];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
