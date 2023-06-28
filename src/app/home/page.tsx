a858c32f66d5b6ad7f27b78693fd52f418a6d3b5
infamous55 <infamouslegend555@gmail.com>
2023-06-28 21:09:36 +0300

    added background blobs

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -49,27 +49,33 @@ export default function Home() {
       <header className="flex w-full items-center justify-center border-b border-gray-200">
         <NavBar />
       </header>
-      <main className="mx-auto w-full md:w-1/3 p-6 flex flex-col items-center justify-center text-center">
-        <h1 className="font-bold text-4xl mb-4">GitHub User Cards</h1>
-        <p className="mb-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sint
-          harum consequatur vel. Ipsum, unde voluptates sit aliquid, eum dolorum
-          quasi quae pariatur facilis.
-        </p>
-        <div className="flex gap-x-4">
-          <button
-            className="px-4 py-2 min-w-[9rem] font-semibold text-white rounded-md shadow-sm bg-red-600 hover:bg-red-500 focus-visible:outline-none focus-visible:bg-red-500"
-            onClick={handleCallToAction}
-          >
-            Get Started
-          </button>
-          <a
-            className="px-4 py-2 min-w-[9rem] font-semibold rounded-md shadow-sm border-2 border-gray-200 focus-visible:outline-none focus-visible:border-red-200"
-            href="https://github.com/infamous55/github-user-cards"
-            target="_blank"
-          >
-            Source Code
-          </a>
+      <main className="relative mx-auto w-11/12 sm:w-4/5 md:w-3/5 lg:w-2/5 flex items-center justify-center">
+        {/* TODO: Dynamically generate multiple blobs to fill the length of main */}
+        <div className="absolute -z-10 left-0 w-2/5 h-72 bg-red-200 rounded-full mix-blend-multiply filter blur-xl opacity-50"></div>
+        <div className="absolute -z-10 w-2/5 h-72 bg-red-200 rounded-full mix-blend-multiply filter blur-xl opacity-50"></div>
+        <div className="absolute -z-10 right-0 w-2/5 h-72 bg-red-200 rounded-full mix-blend-multiply filter blur-xl opacity-50"></div>
+        <div className="w-full h-full border border-gray-200 rounded-sm shadow-sm bg-white backdrop-blur-sm bg-opacity-40 p-6 flex flex-col items-center justify-center text-center">
+          <h1 className="font-bold text-4xl mb-4">GitHub User Cards</h1>
+          <p className="mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sint
+            harum consequatur vel. Ipsum, unde voluptates sit aliquid, eum
+            dolorum quasi quae pariatur facilis.
+          </p>
+          <div className="flex gap-x-4">
+            <button
+              className="px-4 py-2 min-w-[9rem] font-semibold text-white rounded-md shadow-sm bg-red-600 hover:bg-red-500 focus-visible:outline-none focus-visible:bg-red-500"
+              onClick={handleCallToAction}
+            >
+              Get Started
+            </button>
+            <a
+              className="px-4 py-2 min-w-[9rem] font-semibold rounded-md shadow-sm border-2 bg-white border-gray-200 focus-visible:outline-none focus-visible:border-red-200"
+              href="https://github.com/infamous55/github-user-cards"
+              target="_blank"
+            >
+              Source Code
+            </a>
+          </div>
         </div>
       </main>
       <footer className="bg-black text-white  w-full">
